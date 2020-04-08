55b718d6ad8d4fba176727ec83873eea692bac7d
lapiequichante86 <55737704+lapiequichante86@users.noreply.github.com>
2020-04-08 15:32:40 +0200

    Add files via upload

--- exo_07_ c.rb
+++ exo_07_ c.rb
@@ -1,5 +1,5 @@
 user_name = gets.chomp
 puts user_name
 
-# dans le b une ligne est rajouté  mais ne fonctionne pas 
-# dans le c les deux premieres ligne de l'exerciuce précédent sont supprimé et ça marche encore moins
+# dans le b une ligne print indique à la personne où taper et qu'il y  a une réponse, elle sera tapé à la suite dans la même ligne
+# dans le c aucune indication à l'utilisateur
